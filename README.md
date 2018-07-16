9856bc0f2a16421d84360874dbe9ed40d7451f5d
atakanargn <argin.atakan@gmail.com>
2018-07-16 15:39:00 +0300

    removed api.txt and pyautogui library.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # TSH-Protocol
-Create bot from BotFather and add api key to api.txt.
+## Latest update
+* api.txt removed, Api Key must add to TSH.py "apiKey" variable.
+* requirements.txt updated.
+* Unnecessary library's removed (pyautogui)
+* * Pyautogui removed because this library for take screenshot from bot computer but if this computer don't have desktop environment or windows manager, pyautogui isn't working properly.
+
+## Usage
+0) First give "pip install -r requirements.txt" command from Terminal.
+1) Create Telegram bot with @BotFather
+2) Copy your Api Key and paste to apiKey variable.
+3) Run TSH.py
+4)  "/start" command will start your conversation with your bot.
+    "/t Terminal Command" will run your commands
+5) That's it!
+
+If you wanna start this bot at every startup;
+give "crontab -e" command and add this line,
+* @reboot cd ~/TSH-Protocol/ && python TSH.py
--- TSH.py
+++ TSH.py
@@ -1,75 +1,42 @@
+# Author   : Atakan Argın
+# Website  : https://atakanargn.blogspot.com/
+# Linkedin : https://linkedin.com/in/atakanargn
+# Github   : https://github.com/atakanargn
+
 # - *- coding: utf- 8 - *-
 from telegram.ext import Updater, CommandHandler
 import os
-import pyautogui as pag
 
 ### SETTINGS ###
 
 # BOT API TOKEN
-config  = open("api.txt","r")
-apiKey  = config.read()
-config.close()
-
-# START PATH
-START_PATH = str(os.getcwd()+"/")
-
-# HOME PATH
-HOME_PATH = os.path.expanduser('~')
+apiKey  = "YOUR-BOT'S-API-TOKEN"
 
 # /START COMMAND
 def start(bot, update):
-    update.message.reply_text("""Hi!\nYoda is my name!\n\
-If control this computer you wanna;\n login with this command you can:\n/login username password\n""")
-
-# /HELP COMMAND
-def helpFunc(bot,update):
-    update.message.reply_text("Available Commands;\n\
-/t terminalCommand >> shell any terminal command, you can with this command!\n\
-/w words >> write like keyboard\n\
-/screen >> Screenshot from computer\n\
-/help >> All of commands!\n\
-")
+    update.message.reply_text("TERMINAL ENABLED!")
 
 # /T COMMAND
 def terminalFunc(bot,update,args):
     incomeText = " ".join(args)
-    os.system(incomeText+str("  | tee "+START_PATH+"lastCommands.txt"))
-    sCmdFile = open(START_PATH+"lastCommands.txt")
-    update.message.reply_text("Your Command : \""+incomeText+"\"\n\n\
-Output ;\n\n"+sCmdFile.read())
+    os.system(incomeText+str("  | tee lastCommands.txt"))
+    sCmdFile = open("lastCommands.txt")
+    update.message.reply_text(str(sCmdFile.read()))
     sCmdFile.close()
-    os.system("rm "+START_PATH+"lastCommands.txt")
-
-# /W COMMAND
-def writeFunc(bot,update,args):
-    incomeText = " ".join(args)
-    pag.typewrite(incomeText)
-
-# /SCREEN COMMAND
-def screenshotFunc(bot,update):
-    pic = pag.screenshot()
-    pic.save("ss.png")
-    bot.send_photo(chat_id=update.message.chat_id, photo=open("ss.png","rb"))
-    os.system("rm ss.png")
+    os.system("rm lastCommands.txt")
 
 def main():
     updater = Updater(token=apiKey)
     dispatcher = updater.dispatcher
-    print("Bot started...\n\tyour commands from Telegram, Awaiting, I am!")
+    print("BOT STARTED!")
 
     ### BOT COMMANDS
     start_handler = CommandHandler('start',start)
-    help_handler = CommandHandler('help',helpFunc)
     terminal_handler = CommandHandler('t',terminalFunc,pass_args=True)
-    write_handler = CommandHandler('w',writeFunc,pass_args=True)
-    screen_handler = CommandHandler('screen',screenshotFunc)
 
     # COMMAND DISPATCH
     dispatcher.add_handler(start_handler)
-    dispatcher.add_handler(help_handler)
     dispatcher.add_handler(terminal_handler)
-    dispatcher.add_handler(write_handler)
-    dispatcher.add_handler(screen_handler)
 
     # START THE BOT
     updater.start_polling()
